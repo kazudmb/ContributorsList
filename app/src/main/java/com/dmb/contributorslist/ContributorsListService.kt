eabcb79491cb55e4a8ba7b8bcb1f559e08d32a5c
kazudmb <nakano.kazuki.dmb@gmail.com>
2021-08-04 01:34:42 +0900

    add Repositoryの追記

---
+++ app/src/main/java/com/dmb/contributorslist/ContributorsListService.kt
@@ -0,0 +1,18 @@
+package com.dmb.contributorslist
+
+import com.dmb.contributorslist.Project
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ContributorsListService {
+
+    //一覧
+    @GET("users/{user}/repos")
+    suspend fun getProjectList(@Path("user") user: String): Response<List<Project>>
+
+    //詳細
+    @GET("/repos/{user}/{reponame}")
+    suspend fun getProjectDetails(@Path("user") user: String, @Path("reponame") projectName: String): Response<Project>
+}
