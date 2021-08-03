7c82eee681122d745a2afd48efd34a1314223316
kazudmb <nakano.kazuki.dmb@gmail.com>
2021-08-04 01:34:24 +0900

    add Modelを作成

---
+++ app/src/main/java/com/dmb/contributorslist/User.kt
@@ -0,0 +1,22 @@
+package com.dmb.contributorslist
+
+data class User(
+    val login: String,
+    var id: Long,
+    var node_id: String,
+    var avatar_url: String,
+    var gravatar_id: String,
+    var url: String,
+    var html_url: String,
+    var followers_url: String,
+    var following_url: String,
+    var gists_url: String,
+    var starred_url: String,
+    var subscriptions_url: String,
+    var organizations_url: String,
+    var repos_url: String,
+    var events_url: String,
+    var received_events_url: String,
+    var type: Boolean,
+    var site_admin: Int,
+)
